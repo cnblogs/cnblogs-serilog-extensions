8a474118501d9ca0c1eb9d488e2f1271d9c1b463
dudu <dudu@cnblogs.com>
2023-02-12 17:10:05 +0800

    chore: rename Serilog.Extensions.Hosting to Cnblogs.Serilog.Extensions

--- pack.sh
+++ pack.sh
@@ -4,6 +4,6 @@ set -e
 [ -z $1 ] && echo "Missing version" && exit 1
 
 version=$1
-project=src/Serilog.Extensions.Hosting
+project=src/Cnblogs.Serilog.Extensions
 dotnet build -p:Version=${version-*} -c Release $project 
 dotnet pack $project -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg --include-source -p:PackageVersion=$version -p:Version=${version-*} -o ./artifacts
