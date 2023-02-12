10c4e3e5256e5fbbfc3fe58af5b2880a05bf9031
dudu <dudu@cnblogs.com>
2023-02-12 09:05:59 +0800

    test: add a test case for AddSerilog

---
+++ test/Serilog.Extensions.Hosting.Tests/SerilogLoggingBuilderExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Logging;
+using Serilog.Sinks.InMemory;
+using Serilog.Sinks.InMemory.Assertions;
+using Xunit;
+
+namespace Serilog.Extensions.Hosting.Tests;
+
+public class SerilogLoggingBuilderExtensionsTests
+{
+    [Fact]
+    public async Task LoggingBuilderExtensions_AddSerilog_SuccessAsync()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+        var logger = new LoggerConfiguration()
+            .WriteTo.InMemory()
+            .CreateLogger();
+        builder.Logging.AddSerilog(logger);
+        builder.WebHost.UseTestServer();
+        var app = builder.Build();
+
+        // Act
+        var message = "Hello World!";
+        app.Logger.LogInformation("Hello World!");
+        await app.StartAsync();
+
+        // Assert
+        InMemorySink.Instance.Should().HaveMessage(message);
+    }
+}
